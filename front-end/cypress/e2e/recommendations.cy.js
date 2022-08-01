39ede12f49da7e80e1e0b6b16d7d517422414da7
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-08-01 16:18:53 -0300

    feat: feito testes e2e para top e random recommendations

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -4,7 +4,6 @@ import { faker } from '@faker-js/faker'
 const URL = "http://localhost:5000"
 
 describe("User flow", () => {
-
     it("Post a recommendation", async () => {
         const recommendation = {
             name: faker.name.findName(),
@@ -19,6 +18,28 @@ describe("User flow", () => {
         cy.get("#send").click()
         cy.wait("@createRecommendation").then(({ response }) => {
             expect(response.statusCode).equal(201)
+        })
+    })
+
+    it("Get top recommendations", async () => {
+        cy.visit(URL)
+        cy.intercept("GET", `${URL}/recommendations/top/10`).as(
+            "getRecommendations"
+        )
+        cy.get("#top").click()
+        cy.wait("@getRecommendations").then(({ response }) => {
+            expect(response.statusCode).equal(200)
+        })
+    })
+
+    it("Get random recommendation", async () => {
+        cy.visit(URL)
+        cy.intercept("GET", `${URL}/recommendations/random`).as(
+            "randomRecommendations"
+        )
+        cy.get("#random").click()
+        cy.wait("@randomRecommendations").then(({ response }) => {
+            expect(response.statusCode).equal(200)
         })
     })
 })
